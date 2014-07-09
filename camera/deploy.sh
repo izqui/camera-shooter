4531b7d5da8639b97e2d2152a668bf9729ab3b20
Jorge Izquierdo <jorge@izqui.me>
2014-07-09 14:16:22 -0400

    Camera bot working

---
+++ camera/deploy.sh
@@ -0,0 +1,2 @@
+GOARCH=arm GOOS=linux go build
+scp camera-shooter pi@10.0.5.195:camera/shooter_fast
--- deploy.sh
+++
@@ -1,2 +0,0 @@
-GOARCH=arm GOOS=linux go build
-scp camera-shooter pi@10.0.5.195:camera/shooter_fast
